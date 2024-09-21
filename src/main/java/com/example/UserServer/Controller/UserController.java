2bf7c1fcbb1a4d4c5821c515e4fb2fce6c5c295c
shakeeb.raza <sja@eclear.com>
2024-09-21 19:30:49 +0200

    updated to java 21

--- src/main/java/com/example/UserServer/Controller/UserController.java
+++ src/main/java/com/example/UserServer/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.UserServer.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +14,13 @@ public class UserController {
     @Autowired
     Environment env;
 
+    @Value("${spring.application.profile}")
+    String profile;
+
     @GetMapping(value = "/user-test")
     public String test()
     {
-    return "User Working on Port: "+env.getProperty("local.server.port");
+    return "User Working on Port: "+env.getProperty("local.server.port")+", profile: "+profile ;
     }
 
 
