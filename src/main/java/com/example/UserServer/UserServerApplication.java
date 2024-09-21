74c0f3e2bc2098b6db2dc0e1a47163f160ac3dd7
shakeeb.raza <sja@eclear.com>
2024-09-21 19:30:49 +0200

    updated to java 21

--- src/main/java/com/example/UserServer/UserServerApplication.java
+++ src/main/java/com/example/UserServer/UserServerApplication.java
@@ -2,12 +2,10 @@ package com.example.UserServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EnableDiscoveryClient
+@RibbonClient(name = "UserServer") // Specify the service name for load balancing
 public class UserServerApplication {
 
 	public static void main(String[] args) {
